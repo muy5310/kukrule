e9d2597eccd9809c48ba0ead65d5898646c8a0dc
muy5310 <muy5310@naver.com>
2021-01-27 17:50:32 +0900

    item color

--- src/components/SurveyItem.vue
+++ src/components/SurveyItem.vue
@@ -48,9 +48,12 @@ export default {
     height: 30px;
     margin-bottom:10px;
     border:none;
-    background-image: linear-gradient(to top, #fdcbf1 0%, #fdcbf1 1%, #f1e7f5 100%);
+    background-image: linear-gradient(to top, #fdf3a8 0%, #fff081 1%, #fffb83 100%);
+    /* 핑크색 */
+    /* background-image: linear-gradient(to top, #fdcbf1 0%, #fdcbf1 1%, #f1e7f5 100%); */
     /* 노랑색  */
     /* background-image: linear-gradient(to top, #fae924 0%,  #fefa80 100%); */
+    /* 파랑색 */
     /* background: linear-gradient(to top, #a1c4fd 0%, #c2e9fb 100%); */
     border-radius:3px;
 	}
