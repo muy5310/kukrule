466116ec985f919f45c69918478ecd8f4c6472ac
muy5310 <muy5310@naver.com>
2021-02-09 15:50:39 +0900

    data change

--- src/assets/postData.js
+++ src/assets/postData.js
@@ -1,10 +1,12 @@
+import cleanimg from '../image/whoiscleaner.jpg';
+
 export default [{
-        title: '불독 귀엽다 vs 아니다',
-        author: '푸들러버',
+        title: '샤워는 하루에 한번만!이면 언제 해야 함?',
+        author: '깔끔요정',
         date: '2021.02.21',
-        img: 'https://cdn.pixabay.com/photo/2014/12/10/05/50/english-bulldog-562723_960_720.jpg',
-        caption: '난 개인적으로 불독 귀여운지 모르겠는데 이 사진 귀엽다 생각하는 사람?',
-        items: ['귀엽다', '뭐가 귀여운지 모르겠다', '그냥 아무생각 없음'],
+        img: cleanimg,
+        caption: '라떼는 말이야 하루 한번도 과분해',
+        items: ['자기 전 샤워', '일어나서 샤워', '기타 나만의 방식'],
         liked: false,
         postlike: 95,
         moreClick: false,
@@ -13,17 +15,55 @@ export default [{
         imgShown: true
     },
     {
-        title: '사진 분위기 ',
-        author: 'photographer',
-        date: '2021.02.19',
-        img: 'https://cdn.pixabay.com/photo/2019/07/29/06/01/holiday-4369987_960_720.jpg',
-        caption: '이 사진땜에 친구랑 싸우는 중인데 저 사진 그냥 여유로워 보이고 아름다운 느낌 아닌가? 쓸쓸해 보이고 슬퍼 보인다 생각하는 사람도 있어? 투표 ㄱㄱㄱ',
-        items: ['쓸쓸함', '아름다움'],
+        title: '밥 먹다가 친구가 냅킨 한장만 하면 몇 장 줘?',
+        author: '통째로',
+        date: '2021.02.21',
+        caption: '한장 달라고 해서 한장 줬더니 센스없는 넘이라고 욕 먹음',
+        items: ['1장', '2장', '3장 이상', '냅킨 통째로'],
         liked: false,
-        postlike: 42,
+        postlike: 95,
         moreClick: false,
         moreShown: true,
         viewmore: true,
-        imgShown: true,
+        imgShown: false
+    },
+    {
+        title: '잘 모르는 회사 동료 축의금 얼마?',
+        author: '비혼주의자',
+        date: '2021.02.21',
+        caption: '일적으로 엮이지도 않고 그냥 얼굴만 아는 사이임',
+        items: ['3만', '5만', '10만', '안한다', '기타'],
+        liked: false,
+        postlike: 95,
+        moreClick: false,
+        moreShown: true,
+        viewmore: true,
+        imgShown: false
+    },
+    {
+        title: '너구리 다시마 먹는 건가요?',
+        author: '오동통',
+        date: '2021.02.21',
+        caption: '너구리에 계란은 안 푸는 건 나도 알아. 그러면 다시마는 먹어???',
+        items: ['먹는다', '안먹는다'],
+        liked: false,
+        postlike: 95,
+        moreClick: false,
+        moreShown: true,
+        viewmore: true,
+        imgShown: false
+    },
+    {
+        title: '소개팅 후 몇번 만에 고백?',
+        author: '고백점프',
+        date: '2021.02.21',
+        caption: '소개팅 해서 만난 애가 있는데 계속 만나기만 하고 얘기안하면 막 고백을 기다리고 그러냐?',
+        items: ['소개팅 날', '소개팅 후 첫번째', '소개팅 후 두번째', '소개팅 후 세번째', '경우에 따라 다름', '기타'],
+        liked: false,
+        postlike: 95,
+        moreClick: false,
+        moreShown: true,
+        viewmore: true,
+        imgShown: false
     }
 ];
